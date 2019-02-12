e0c30c19a97728e5ee5c77379bd1bc61fe42a7cb
justinkim <justinkim@MacBook-Air-6.frontierlocal.net>
2019-02-12 17:13:58 -0600

    I did this

--- src/assignment_2.1.py
+++ src/assignment_2.1.py
@@ -1,3 +1,8 @@
 # This first line is provided for you
 
-english = nput("Enter English score:")
+english = input("Enter English score: ")
+math = input('Enter Manth Score: ')
+Total = int(english)+int(math)
+print('Total:', Total)
+Average = Total/2
+print('Average:', Average)
