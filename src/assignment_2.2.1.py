5327c0fede61788e14c32bbaed661ba1aca0a47d
justinkim <justinkim@MacBook-Air-6.frontierlocal.net>
2019-02-12 20:41:06 -0600

    I think I did this. Was this the same as before?

--- src/assignment_2.2.1.py
+++ src/assignment_2.2.1.py
@@ -1,4 +1,11 @@
 # This first 2 lines are provided for you
 
-english = input("Enter English score:")
-h = int(english)
+English = input('Enter English score: ')
+English = int(English)
+Math = input('Enter Math score: ')
+Math = int(Math)
+if English > 80:
+    English = English * 1.5
+total = English + Math
+average = total/2
+print('Average:', average)
